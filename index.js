f8ab925c396686b70fda78a97a0691dc2752a391
EdwinAlmendras <58957716+EdwinAlmendras@users.noreply.github.com>
2020-03-15 20:23:04 +0100

    update index.js

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
 const puppeteer = require('puppeteer');
 
-(async () => {
+(async()=> {
   const browser = await puppeteer.launch();
+
   const page = await browser.newPage();
-  await page.goto('https://example.com');
+  await page.goto('http://dl.free.fr/bVrfLXHub');
 
-  // Get the "viewport" of the page, as reported by the page.
-  const dimensions = await page.evaluate(() => {
-    return {
-      width: document.documentElement.clientWidth,
-      height: document.documentElement.clientHeight,
-      deviceScaleFactor: window.devicePixelRatio
-    };
-  });
 
-  console.log('Dimensions:', dimensions);
+  //console.log(await page.content());
+  //await page.screenshot({path: 'screenshot.png'});
+
+
+
+
+  page.click('input')
+  response.headers()['location']
+  // After
+
+  const chain = response.request().redirectChain();
+  console.log(chain.length); // Return 1
+  console.log(chain[0].url())
+
+  //get download file
+  //like html constantly sending info
+
+  /*
+page.$eval('title', (element) => {
+  return element.innerHTML
+})
+*/
 
   await browser.close();
-})();
+
+
+})()
