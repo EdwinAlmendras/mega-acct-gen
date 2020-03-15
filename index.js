137a2c70e256acb4b3ce4cdc175b7976621db422
EdwinAlmendras <58957716+EdwinAlmendras@users.noreply.github.com>
2020-03-15 20:25:50 +0100

    update index.js

--- index.js
+++ index.js
@@ -1,36 +1,40 @@
 const puppeteer = require('puppeteer');
 
 (async()=> {
-  const browser = await puppeteer.launch();
+  try {
 
-  const page = await browser.newPage();
-  await page.goto('http://dl.free.fr/bVrfLXHub');
+    const browser = await puppeteer.launch();
 
-
-  //console.log(await page.content());
-  //await page.screenshot({path: 'screenshot.png'});
+    const page = await browser.newPage();
+    await page.goto('http://dl.free.fr/bVrfLXHub');
 
 
+    //console.log(await page.content());
+    //await page.screenshot({path: 'screenshot.png'})
 
+    page.click('input')
+    response.headers()['location']
+    // After
 
-  page.click('input')
-  response.headers()['location']
-  // After
+    const chain = response.request().redirectChain();
+    console.log(chain.length); // Return 1
+    console.log(chain[0].url())
 
-  const chain = response.request().redirectChain();
-  console.log(chain.length); // Return 1
-  console.log(chain[0].url())
+    //get download file
+    //like html constantly sending info
 
-  //get download file
-  //like html constantly sending info
-
-  /*
+    /*
 page.$eval('title', (element) => {
   return element.innerHTML
 })
 */
 
-  await browser.close();
+    await browser.close();
+
+  }
+  catch (err) {
+    console.log(err)
+  }
 
 
 })()
