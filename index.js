ea7f9236f3de790b3c873c5effde7c86f82466b6
EdwinAlmendras <58957716+EdwinAlmendras@users.noreply.github.com>
2020-03-15 20:27:30 +0100

    update index.js

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const puppeteer = require('puppeteer');
 (async()=> {
   try {
 
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox']});
 
     const page = await browser.newPage();
     await page.goto('http://dl.free.fr/bVrfLXHub');
