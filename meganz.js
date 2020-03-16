ea5732a1bdfac4889c96637bf838382c99ad4e02
EdwinAlmendras <58957716+EdwinAlmendras@users.noreply.github.com>
2020-03-16 19:50:00 +0100

    update meganz.js

--- meganz.js
+++ meganz.js
@@ -1,34 +1,73 @@
-const puppeteer = require('puppeteer');
-const fs = require('fs');
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+const QrScanner = require('qr-scanner')
+
+
 
 
 (async () => {
   const browser = await puppeteer.launch();
+
+  const pageEmail = await browser.newPage(); // open new tab
+
+  const urlEmail = 'https://temp-mail.org/en'
+  await pageEmail.goto(url, {
+    waitUntil: "networkidle0",
+  });
+
+  pageEmail.click('button[data-base-url="https://temp-mail.org/en"]')
+
+
+  const qrImageBuffer = await pageEmail.screenshot()
+
+
+
+
+  QrScanner.scanImage(qrImageBuffer)
+  .then(result => console.log(result))
+  .catch(error => console.log(error))
+
+
+
   const page = await browser.newPage();
-  const url = 'https://mega.nz/register';
+  const url = "https://mega.nz/register";
 
   await page.goto(url, {
-    waitUntil: 'networkidle0',
+    waitUntil: "networkidle0",
   });
+
+  await page.bringToFront();
+
+  //name email password
+
+  await page.type("input[name='register-name2']", name)
+  await page.type("input[name='register-familyname2']", lastName)
+  await page.type("input[name='register-email2']", email)
+  await page.type("input[name='register-passwor2']", password)
+  await page.type("input[name='register-password3']", password)
+
+  await page.click("div[class='understand-check checkboxOff checkbox'] input")
+  await page.click("div[class='register-check checkboxOff checkbox'] input")
+  /*
+await page.click("input class=[big-red-button height-48 register-button right button active]")
+*/
+  //sucess register
+
+
 
   // Get the "viewport" of the page, as reported by the page.
-  /* const dimensions = await page.evaluate(() => {
-    return {
-      width: document.documentElement.clientWidth,
-      height: document.documentElement.clientHeight,
-      deviceScaleFactor: window.devicePixelRatio
-    };
-  });
 
-  console.log('Dimensions:', dimensions);*/
 
-  //  await page.type('#mytextarea', 'Hello')
+  /*
+  console.log("Dimensions:", dimensions);*/
+
+  //  await page.type("#mytextarea", "Hello")
   //const html = await page.content()
   /*
   const elem = await page.$eval("input", (element) => {
     return element.innerHTML
-  })*/
-  const inputs = await page.evaluate(() => Array.from(document.querySelectorAll('form'), element => element.innerHTML));
+  })
+  const inputs = await page.evaluate(() => Array.from(document.querySelectorAll("form"), element => element.innerHTML));
   inputs.forEach(el => {
     console.log(el)})
 
@@ -39,6 +78,6 @@ const fs = require('fs');
     console.log("The file was saved!");
   });
 
-
+*/
   await browser.close();
 })();
