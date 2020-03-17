c29a03f7b939430adde14247ce151129ba42597c
EdwinAlmendras <58957716+EdwinAlmendras@users.noreply.github.com>
2020-03-17 11:33:41 +0100

    update meganz.js

--- meganz.js
+++ meganz.js
@@ -99,7 +99,7 @@ const randomstring = require("randomstring");
   await pageEmail.type('input[name="login-password3"]', md5(email))
   await pageEmail.click('div[class="big-red-button height-48 top-dialog-login-button button right"]')
 
-  await pageEmail.waitFor(2000)
+  //await pageEmail.waitFor(2000)
 
   console.log(await pageEmail.url())
 
