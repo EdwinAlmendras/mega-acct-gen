780c788b90fe72e91974fc2986835c24ae45efd2
EdwinAlmendras <58957716+EdwinAlmendras@users.noreply.github.com>
2020-03-19 17:59:47 +0100

    update meganz.js

--- meganz.js
+++ meganz.js
@@ -1,15 +1,15 @@
 const puppeteer = require("puppeteer");
 const fs = require("fs");
 const axios = require("axios");
-var md5 = require('md5');
+const md5 = require('md5');
 const randomstring = require("randomstring");
-var  PastebinAPI = require ('pastebin-js');
+const  PastebinAPI = require ('pastebin-js');
+const  moment = require ('moment');
 
-
+//PASTEBIN SETTINGS
 const API_KEY = '66797adb0b23b070bb4019851a1b1122'
 const USER = 'gxldxm689171'
 const PSW = 'bea54436fabf243c24b767289fbdf05f'
-
 
 pastebin = new PastebinAPI({
   'api_dev_key': API_KEY,
@@ -18,108 +18,163 @@ pastebin = new PastebinAPI({
 
 });
 
-(async () => {
-  const browser = await puppeteer.launch();
 
-  const pageEmail = await browser.newPage(); // open new tab
+inquirer
+.prompt([{
+  type: "number",
+  name: "accts",
+  message: "Which accounts can you create?",
+  default: 1
+  }])
+  .then(async answers => {
+    //:
+    //email:password
+    const megaAccounts = ''
+    const numberOfAccounts = accounts.accts
 
-  const urlEmail = 'https://www.fakemailgenerator.net/'
-  await pageEmail.goto(urlEmail, {
-    waitUntil: "networkidle0",
-  });
+    console.log('creating mega accounts.. please wait')
 
-
-  const email = await pageEmail.$eval('#active-mail', el => el.getAttribute('data-clipboard-text'))
-  const password = md5(email)
-
-  const name = randomstring.generate(5)
-  const lastName = randomstring.generate(7)
-
-  console.log(email)
-  console.log(password)
-
-  const pathEmail = email.replace(/\@(.*)/g, "")
-
-
-  const page = await browser.newPage();
-  const url = "https://mega.nz/register";
-
-  await page.goto(url, {
-    waitUntil: "networkidle0",
-  });
-
-
-  await page.bringToFront();
-
-  console.log(`starting writting data to mega.nz/register`)
-
-  await page.type("input[name='register-name2']", name)
-  await page.type("input[name='register-familyname2']", lastName)
-  await page.type("input[name='register-email2']", email)
-  await page.type("input[name='register-password2']", password)
-  await page.type("input[name='register-password3']", password)
-
-  await page.click("div[class='understand-check checkboxOff checkbox'] input")
-  await page.click("div[class='register-check checkboxOff checkbox'] input")
-
-
-  await page.click("div[class='big-red-button height-48 register-button right button active']")
-
-  console.log(`sending all data to MEGA.nz`)
-  await pageEmail.bringToFront();
-
-
-
-
-  await pageEmail.waitFor(2000)
-
-
-
-
-  async function getEmailId() {
-    try {
-      const response = await axios.get('https://www.fakemailgenerator.net/api/v1/mailbox/' + pathEmail);
-
-      console.log(`Getting the emails by fakeeamilgenerator API v1`)
-      const emails = response.data
-      console.log(email[0].id)
-      return emails[0].id
-
-    } catch (error) {
-      console.error(error);
+    for (i = 0; i < numberOfAccounts; i++) {
+      const accountMega = await createMegaAccount()
+      megaAccounts.concat(accountMega + '/n')
     }
+    await createPaste(megaAccounts, moment().format('MMMM Do YYYY, h:mm:ss a'))
+
+    console.log('created paste with this accounts: /n' + megaAccounts)
+  })
+
+  /*
+const forLoop = async _ => {
+  console.log('Start')
+
+  for (let index = 0; index < fruitsToGet.length; index++) {
+    const fruit = fruitsToGet[index]
+    const numFruit = await getNumFruit(fruit)
+    console.log(numFruit)
   }
-  const id = await getEmailId()
+
+  console.log('End')
+}*/
+
+  async function createMegaAccount() {
+    const browser = await puppeteer.launch();
+
+    const pageEmail = await browser.newPage(); // open new tab
+
+    const urlEmail = 'https://www.fakemailgenerator.net/'
+    await pageEmail.goto(urlEmail,
+      {
+        waitUntil: "networkidle0",
+      });
+
+
+    const email = await pageEmail.$eval('#active-mail',
+      el => el.getAttribute('data-clipboard-text'))
+    const password = md5(email)
+
+    const name = randomstring.generate(5)
+    const lastName = randomstring.generate(7)
+
+    const pathEmail = email.replace(/\@(.*)/g,
+      "")
+
+
+    const page = await browser.newPage();
+    const url = "https://mega.nz/register";
+
+    await page.goto(url,
+      {
+        waitUntil: "networkidle0",
+      });
+
+
+    await page.bringToFront();
+
+    console.log(`starting writting data to mega.nz/register`)
+
+    await page.type("input[name='register-name2']",
+      name)
+    await page.type("input[name='register-familyname2']",
+      lastName)
+    await page.type("input[name='register-email2']",
+      email)
+    await page.type("input[name='register-password2']",
+      password)
+    await page.type("input[name='register-password3']",
+      password)
+
+    await page.click("div[class='understand-check checkboxOff checkbox'] input")
+    await page.click("div[class='register-check checkboxOff checkbox'] input")
+
+
+    await page.click("div[class='big-red-button height-48 register-button right button active']")
+
+    console.log(`sending all data to MEGA.nz`)
+    await pageEmail.bringToFront();
 
 
 
-  const emailLink = `https://www.fakemailgenerator.net/mailbox/${pathEmail}/${id}`
-  console.log(`going to url of email...`)
-  console.log(emailLink)
 
-  await pageEmail.goto(emailLink, {
-    waitUntil: "networkidle0",
-  });
+    await pageEmail.waitFor(2000)
 
-  console.log(`Getting confirm link`)
 
-  const link = await pageEmail.$eval('#bottom-button', el => el.getAttribute('href'))
 
-  await pageEmail.goto(link, {
-    waitUntil: "networkidle0",
-  });
 
-  console.log('sucessfully create account with' + email)
+    async function getEmailId() {
+      try {
+        const response = await axios.get('https://www.fakemailgenerator.net/api/v1/mailbox/' + pathEmail);
 
-  pastebin
-  .createPaste(email, "email-account", null, 3)
-  .then(function (data) {
-    console.log(data);
-  })
-  .fail(function (err) {
-    // Something went wrong
-    console.log(err);
-  })
+        console.log(`Getting the emails by fakeeamilgenerator API v1`)
+        const emails = response.data
+        console.log(email[0].id)
+        return emails[0].id
 
-  await browser.close();
-})();
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    const id = await getEmailId()
+
+
+
+    const emailLink = `https://www.fakemailgenerator.net/mailbox/${pathEmail}/${id}`
+    console.log(`going to url of email...`)
+    console.log(emailLink)
+
+    await pageEmail.goto(emailLink,
+      {
+        waitUntil: "networkidle0",
+      });
+
+    console.log(`Getting confirm link`)
+
+    const link = await pageEmail.$eval('#bottom-button',
+      el => el.getAttribute('href'))
+
+    await pageEmail.goto(link,
+      {
+        waitUntil: "networkidle0",
+      });
+
+
+    return email+":"+password
+
+    await browser.close();
+  }
+
+
+  function createPaste(data, title) {
+    //data, title
+    pastebin
+    .createPaste(data,
+      title,
+      null,
+      2)
+    .then(function (data) {
+      console.log('sucessfully create paste')
+    })
+    .fail(function (err) {
+      // Something went wrong
+      console.log(err);
+    })
+  }
