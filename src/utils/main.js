8cb700f925f470267782a993f31624281e18c68b
EdwinAlmendras <58957716+EdwinAlmendras@users.noreply.github.com>
2020-04-06 22:52:45 +0200

    update main.js

--- src/utils/main.js
+++ src/utils/main.js
@@ -5,48 +5,65 @@ import createPaste from './createPaste'
 import createMegaAccount from './createMegaAccount'
 
 
-const main = async () => {
 
-  const answers = await inquirer.prompt(questions)
-
-
-  //Answers
-  let numberOfAccounts = answers.accts
-  let res = answers.question
-
-
-  let megaAccounts = '';
-  let accountMega;
-
-  console.log('creating mega accounts.. please wait')
+let questions = [{
+  type: "number",
+  name: "accts",
+  message: "Which accounts can you create?",
+  default: 1
+  },
+  {
+    type: "confirm",
+    name: "question",
+    message: "You can hash password(email)?"
+  }]
 
 
-  for (i = 0; i < numberOfAccounts; i++) {
 
-    if (res) {
-      accountMega = await
-      createMegaAccount(true)
-    } else {
-      accountMega = await
-      createMegaAccount(false)
+
+
+  const main = async () => {
+
+    const answers = await inquirer.prompt(questions)
+
+
+    //Answers
+    let numberOfAccounts = answers.accts
+    let res = answers.question
+
+
+    let megaAccounts = '';
+    let accountMega;
+
+    console.log('creating mega accounts.. please wait')
+
+
+    for (i = 0; i < numberOfAccounts; i++) {
+
+      if (res) {
+        accountMega = await
+        createMegaAccount(true)
+      } else {
+        accountMega = await
+        createMegaAccount(false)
+      }
+      megaAccounts += accountMega + '\n'
     }
-    megaAccounts += accountMega + '\n'
-  }
 
 
-  let date = await moment().format('MMMM Do
-    YYYY, h: mm: ss a')
-    let link;
+    let date = await moment().format('MMMM Do
+      YYYY, h: mm: ss a')
+      let link;
 
-    try {
-      link = await createPaste(megaAccounts, date)
+      try {
+        link = await createPaste(megaAccounts, date)
+      }
+      catch(err) {
+        console.log('something wrong saving to pastebin')}
+
+
+      console.log('sucess saved\n link:', link)
     }
-    catch(err) {
-      console.log('something wrong saving to pastebin')}
 
 
-    console.log('sucess saved\n link:', link)
-  }
-
-
-  export default main;
+    export default main;
