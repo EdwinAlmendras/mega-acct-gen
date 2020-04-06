3388b4767dfe4770ebb21673a9e4be33747b79c5
EdwinAlmendras <58957716+EdwinAlmendras@users.noreply.github.com>
2020-04-06 19:41:30 +0200

    update main.js

--- src/utils/main.js
+++ src/utils/main.js
@@ -1,41 +1,42 @@
-  
+import inquirer from 'inquirer'
+
+import createPaste from './createPaste'
+import createMegaAccount from './createMegaAccount'
+
+
 const main = async () => {
-  
+
   const answers = await inquirer.prompt(questions)
-  
-  
-  
+
   const numberOfAccounts = answers.accts
   const res = answers.question
   let megaAccounts = '';
   let accountMega;
-  
-  console.log('creating mega accounts.. please 
-wait')
-  
-  
+
+  console.log('creating mega accounts.. please wait')
+
+
   for (i = 0; i < numberOfAccounts; i++) {
-    
-      if (res) {
-        accountMega = await 
-createMegaAccount(true)
-      }
-      else {
-        accountMega = await 
-createMegaAccount(false)
-        
-      }
-      
-      megaAccounts += accountMega + '\n'
+
+    if (res) {
+      accountMega = await
+      createMegaAccount(true)
+    } else {
+      accountMega = await
+      createMegaAccount(false)
+
+    }
+
+    megaAccounts += accountMega + '\n'
   }
-    const date = await moment().format('MMMM Do 
-YYYY, h:mm:ss a')
-    
+  const date = await moment().format('MMMM Do
+    YYYY, h: mm: ss a')
+
     await createPaste(megaAccounts, date)
-    
-    
+
+
     console.log('sucess')
-}
+  }
 
 
-export default main;
+  export default main;
