dd609d9b6cd50cd31f504c3cb89e3f4075dad1a7
EdwinAlmendras <58957716+EdwinAlmendras@users.noreply.github.com>
2020-04-06 19:02:57 +0200

    update index.js

--- src/index.js
+++ src/index.js
@@ -214,39 +214,3 @@ inquirer
       }
     }
   
-  
-  const signupInputMegaHandler = async (browser)=>{
-    const page = await browser.newPage();
-    const url = "https://mega.nz/register";
-
-    await page.goto(url,
-      {
-        waitUntil: "networkidle0",
-      });
-
-
-    await page.bringToFront();
-
-    console.log(`starting writting data to mega.nz/register`)
-
-    await page.type("input[name='register-name2']",
-      name)
-    await page.type("input[name='register-familyname2']",
-      lastName)
-    await page.type("input[name='register-email2']",
-      email)
-    await page.type("input[name='register-password2']",
-      password)
-    await page.type("input[name='register-password3']",
-      password)
-
-    await page.click("div[class='understand-check checkboxOff checkbox'] input")
-    await page.click("div[class='register-check checkboxOff checkbox'] input")
-
-
-    await page.click("div[class='big-red-button height-48 register-button right button active']")
-
-    console.log(`sending all data to MEGA.nz`)
-    
-    
-  }
