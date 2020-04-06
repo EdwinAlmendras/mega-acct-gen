90012b3dbbf993b9cf8048389b8fdfd713e97d05
EdwinAlmendras <58957716+EdwinAlmendras@users.noreply.github.com>
2020-04-06 23:25:35 +0200

    update megaRegister.js

--- src/utils/megaRegister.js
+++ src/utils/megaRegister.js
@@ -1,38 +1,48 @@
-const megaRegister = async (browser, name, lastName, email, password)=>{
-    const page = await browser.newPage();
-    const url = "https://mega.nz/register";
+const megaRegister = async (browser, user)=> {
 
-    await page.goto(url,
-      {
-        waitUntil: "networkidle0",
-      });
-
-
-    await page.bringToFront();
-
-    console.log(`starting writting data to mega.nz/register`)
-
-    await page.type("input[name='register-name2']",
-      name)
-    await page.type("input[name='register-familyname2']",
-      lastName)
-    await page.type("input[name='register-email2']",
-      email)
-    await page.type("input[name='register-password2']",
-      password)
-    await page.type("input[name='register-password3']",
-      password)
-
-    await page.click("div[class='understand-check checkboxOff checkbox'] input")
-    await page.click("div[class='register-check checkboxOff checkbox'] input")
+  const {
+    name,
+    lastName,
+    email,
+    password
+  } = user
 
 
-    await page.click("div[class='big-red-button height-48 register-button right button active']")
 
-    console.log(`registering ... => MEGA.nz`)
-    
-    
-  }
-  
-  
-  export default megaRegister;
+  const page = await browser.newPage();
+  const url = "https://mega.nz/register";
+
+  await page.goto(url,
+    {
+      waitUntil: "networkidle0",
+    });
+
+
+  await page.bringToFront();
+
+  console.log(`starting writting data to mega.nz/register`)
+
+  await page.type("input[name='register-name2']",
+    name)
+  await page.type("input[name='register-familyname2']",
+    lastName)
+  await page.type("input[name='register-email2']",
+    email)
+  await page.type("input[name='register-password2']",
+    password)
+  await page.type("input[name='register-password3']",
+    password)
+
+  await page.click("div[class='understand-check checkboxOff checkbox'] input")
+  await page.click("div[class='register-check checkboxOff checkbox'] input")
+
+
+  await page.click("div[class='big-red-button height-48 register-button right button active']")
+
+  console.log(`registering ... => MEGA.nz`)
+
+
+}
+
+
+export default megaRegister;
