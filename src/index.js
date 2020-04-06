7a3261dd3389f5844307be48231a31db2febc09f
EdwinAlmendras <58957716+EdwinAlmendras@users.noreply.github.com>
2020-04-06 22:52:36 +0200

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,1 @@
-import faker from 'faker'
-import fs from 'fs'
-import axios from 'axios'
-import md5 from 'md5'
-import PastebinAPI from 'pastebin-js'
-import moment from 'moment'
-import inquirer from 'inquirer'
-import puppeteer from 'puppeteer'
-
-
-
-
-
-
-
 main()
