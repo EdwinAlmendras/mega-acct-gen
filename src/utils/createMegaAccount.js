b396cd4d8c96162e0b1c423a9c401ea317c0a301
EdwinAlmendras <58957716+EdwinAlmendras@users.noreply.github.com>
2020-04-06 22:48:32 +0200

    update createMegaAccount.js

--- src/utils/createMegaAccount.js
+++ src/utils/createMegaAccount.js
@@ -1,27 +1,42 @@
+import puppeteer from 'puppeteer'
+
+import megaRegister from './megaRegister'
+import getEmailAndUser from './getEmailAndUser'
+
+
 async function createMegaAccount(prop) {
-    
-    
-    const browser = await puppeteer.launch();
-    
-    const pageEmail = await browser.newPage();
-    
-    
-    const { name, lastName, email, password, 
-pathEmail, id}= await 
-getEmailAndFakeData(pageEmail)
-    
-    await signupInputMegaHandler(browser)
-    
-    await pageEmail.bringToFront();
-    await pageEmail.waitFor(2000)
-    
-    await confirmEmail(pageEmail, pathEmail, id)
-      
-      
-    if (prop) {return email+":"+password}
-    if (!prop) {return email}
-    await browser.close();
+
+
+  const browser = await puppeteer.launch();
+
+  const pageEmail = await browser.newPage();
+
+
+  const {
+    name,
+    lastName,
+    email,
+    password,
+    pathEmail,
+    id
+  } = await getEmailAndUser(pageEmail)
+
+  await megaRegister(browser)
+
+  await pageEmail.bringToFront();
+  await pageEmail.waitFor(2000)
+
+  await confirmEmail(pageEmail, pathEmail, id)
+
+
+  if (prop) {
+    return email+":"+password
   }
+  if (!prop) {
+    return email
+  }
+  await browser.close();
+}
 
 
 export default createMegaAccount;
